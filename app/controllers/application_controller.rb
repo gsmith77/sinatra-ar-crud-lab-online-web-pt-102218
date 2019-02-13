5f8b73f371d7a404ad7d395e3f0aea2cd4d7e810
grant smith <grantsmith1256@gmail.com>
2019-02-13 19:01:18 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,16 +18,8 @@ class ApplicationController < Sinatra::Base
 
 
   post '/articles' do
-<<<<<<< HEAD
     article = Article.create(title: params[:title], content: params[:content])
     redirect "/articles/#{article.id}"
-=======
-    #binding.pry
-    Article.create(title: params[:title], content: params[:content])
-    @articles = Article.all
-    erb :index
-    # redirect "/articles/#{article.id}"
->>>>>>> 6370c6746006908ad921fd8a69dd93a1d3cb6352
   end
 
   get '/articles/:id' do
