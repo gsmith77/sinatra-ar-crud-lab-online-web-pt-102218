5fcade2b6c6197fdee59d959ee19a8be663e965a
grant smith <grantsmith1256@gmail.com>
2019-02-13 19:20:25 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,14 +15,14 @@ class ApplicationController < Sinatra::Base
     erb :new
   end
 
-  post '/articles' do
-    article = Article.create(title: params[:title], content: params[:content])
-    redirect "/articles/#{article.id}"
-  end
-
   get '/articles' do
     @articles = Article.all
     erb :index
+  end
+
+  post '/articles' do
+    article = Article.create(title: params[:title], content: params[:content])
+    redirect "/articles/#{article.id}"
   end
 
   get '/articles/:id' do
