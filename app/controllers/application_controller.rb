1e4502f0b6f42511ea5b3594f280960fd7c14f81
grant smith <grantsmith1256@gmail.com>
2019-02-13 19:04:06 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < Sinatra::Base
 
   post '/articles' do
     article = Article.create(title: params[:title], content: params[:content])
-    erb :inde
+    erb :index
   end
 
   get '/articles/:id' do
